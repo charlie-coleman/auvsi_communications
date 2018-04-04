73b36768c374846cee3d14cb75551e457ebdb55d
Charlie Coleman <coleman.charlie97@gmail.com>
2018-04-04 17:56:05 -0500

    Added catches for keyboard interrupts to prevent port 'clogging

--- camera/groundstation.py
+++ camera/groundstation.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import datetime
 
@@ -7,24 +8,30 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 host = socket.gethostname()
-
-port = 3141
+if (len(sys.argv) > 0):
+    port = int(sys.argv[1])
+else:
+    port = 3141
 
 serversocket.bind(('0.0.0.0', port))
 
 serversocket.listen(5)
-while True:
-    clientsocket,addr = serversocket.accept()
+try:
+    while True:
+        clientsocket,addr = serversocket.accept()
 
-    print("Recieved connection, downloading image")
-    filename = "./images/image_"+datetime.datetime.now().strftime("%Y%m%d_%H:%M:%S")+".jpg"
+        print("Recieved connection, downloading image")
+        filename = "./images/image_"+datetime.datetime.now().strftime("%Y%m%d_%H:%M:%S")+".jpg"
 
-    with open(filename, 'wb') as file_to_write:
-        while True:
-            data = clientsocket.recv(1024)
-            if not data:
-                break
-            file_to_write.write(data)
-        file_to_write.close()
-    print('Download Completed')
-    clientsocket.close();
+        with open(filename, 'wb') as file_to_write:
+            while True:
+                data = clientsocket.recv(1024)
+                if not data:
+                    break
+                file_to_write.write(data)
+            file_to_write.close()
+        print('Download Completed')
+        clientsocket.close()
+except KeyboardInterrupt:
+    print('Interrupted')
+    serversocket.close()
--- camera/rpi.py
+++ camera/rpi.py
@@ -3,31 +3,36 @@ import datetime
 import _thread
 import time
 import socket
+import sys
 
 host = socket.gethostname()
-port = 3141
+if (len(sys.argv) > 0):
+    port = sys.argv[0]
+else:
+    port = 3141
 
 def download_thread(lfs):
-	while True:
-		files = [f for f in os.listdir('./images') if os.path.isfile('./images/'+f)]
-		if len(files) < 1:
-			time.sleep(0.5)
-			continue
-		files.sort()
-		file_to_send = files[0]
-		print("SENDING: " + file_to_send)
-		clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		clientsocket.connect(('192.168.0.139', port))
+    try:
+    	while True:
+    		files = [f for f in os.listdir('./images') if os.path.isfile('./images/'+f)]
+    		if len(files) < 1:
+    			time.sleep(0.5)
+    			continue
+    		files.sort()
+    		file_to_send = files[0]
+    		print("SENDING: " + file_to_send)
+    		clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    		clientsocket.connect(('192.168.0.139', port))
 
-		with open("./images/"+file_to_send, 'rb') as file_being_sent:
-			for data in file_being_sent:
-				clientsocket.sendall(data)
-			file_being_sent.close()
-		print("FILE SENT")
-		os.system("mv ./images/"+file_to_send+" ./images/sent/"+file_to_send)
-		clientsocket.close()
-		time.sleep(0.5)
-
+    		with open("./images/"+file_to_send, 'rb') as file_being_sent:
+    			for data in file_being_sent:
+    				clientsocket.sendall(data)
+    			file_being_sent.close()
+    		print("FILE SENT")
+    		os.system("mv ./images/"+file_to_send+" ./images/sent/"+file_to_send)
+    		clientsocket.close()
+    		time.sleep(0.5)
+    except KeyboardInterrupt:
 def input_thread(a_list):
 	input()
 	a_list.append(True)
